d6effaffb3e1b00c17e2db797b45a5e1fe492af4
Chualchai Apichatitiworn <whatbest@MacBook-Pro-khxng-Chualchai.local>
2022-08-12 03:31:08 +0700

    part 4

--- wt-app/components/Weather.js
+++ wt-app/components/Weather.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 import Forecast from "./Forcast";
 import { ImageBackground, StyleSheet, Text , View } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
 
 
 export default function Weather(props) {
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=cfee5859532d1c7d55b6f47165b32dad`)
+                .then((response) => response.json())
+                .then((json) => {
+                     setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp
+                    });
+                })
+                .catch((error) => {
+                    console.warn(error);
+                });
+        }
+    }, [props.zipCode])
     const [forecastInfo, setForecastInfo] = useState({
         main: '-',
         description: '-',
