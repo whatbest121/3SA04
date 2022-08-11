7848fec4645545e883f99d19ab619b79ab0c9e1d
Chualchai Apichatitiworn <whatbest@MacBook-Pro-khxng-Chualchai.local>
2022-08-12 03:15:47 +0700

    part 2.2

--- wt-app/components/Weather.js
+++ wt-app/components/Weather.js
@@ -1,8 +1,29 @@
 import React from "react";
-import { Text } from "react-native";
+import Forecast from "./Forcast";
+import { ImageBackground, StyleSheet, Text , View } from "react-native";
+
 
 export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
+        description: '-',
+        temp: 0
+    })
     return (
-        <Text>{props.zipCode}</Text>
+        <View>
+            <ImageBackground source={require('../Background.jpg')} style={styles.backdrop}>
+                <Text>Zip Code</Text>
+                <Text>{props.zipCode}</Text>
+                <Forecast {...forecastInfo} />
+            </ImageBackground>
+        </View>
     );
 }  
+
+const styles = StyleSheet.create({
+    backdrop: {
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+    },
+}); 
